014a2a03785352b5e39b6713e192d2fb9c2dabf6
Jeroen van de Geest <jeroen@vandegeest.eu>
2024-10-12 10:43:22 +0200

    Add nix package

--- flake.nix
+++ flake.nix
@@ -26,6 +26,36 @@
           ";
         '';
       };
+
+      packages = rec {
+        default = portfolio;
+        portfolio = pkgs.buildNpmPackage {
+          buildInputs =  with pkgs; [ nodejs ];
+          name = "Portfolio";
+          src = ./.;
+
+          npmDepsHash = "sha256-9J30U+i/T8p6XS+vl4yX4jge6HzkMBZIrPeg/95FkeI=";
+          npmBuild = "npm ci;npm run build";
+
+          installPhase = ''
+            echo "Copying files"
+            mkdir -p $out/bin
+            cp -r public $out/
+            cp -r .next $out/
+            cp -r next.config.js $out/
+            cp -r node_modules $out/
+            cp package.json $out/
+
+            echo "#!/bin/sh
+            cd $out
+            ${pkgs.nodejs_22}/bin/npm run start
+            " &> $out/bin/Portfolio
+
+            echo "Changing permissions"
+            chmod +x $out/bin/Portfolio
+          '';
+        };
+      };
     }
   );
 }
